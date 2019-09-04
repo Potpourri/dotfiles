78a5b33b10044d8746276abf8bb7ed280c1f9d24
Potpourri <nikita_belyakov@outlook.com>
2019-09-04 17:06:30 +0300

    :arrow_up: update-pinned-nixpkgs: 0.1.0 → 0.1.1
    - add check of revision don't changed
    - add support to custom paths of nixpkgs-version.json, overlay-version.json
    - remove hardcoded overlay's name (Potpourri)
    - fix check existing env.var. PROJECT_ROOT

--- nixos/nixpkgs/pkgs/update-pinned-nixpkgs/default.nix
+++ nixos/nixpkgs/pkgs/update-pinned-nixpkgs/default.nix
@@ -1,15 +1,18 @@
 { stdenv
 , nix-prefetch-git
+, jq
 }:
 
 stdenv.mkDerivation rec {
   name = "update-pinned-nixpkgs";
-  version = "0.1.0";
+  version = "0.1.1";
   src = ./.;
 
   patchPhase = ''
     substituteInPlace update-pinned-nixpkgs --replace 'nix-prefetch-git' \
       '${nix-prefetch-git}/bin/nix-prefetch-git'
+    substituteInPlace update-pinned-nixpkgs --replace 'jq' \
+      '${jq}/bin/jq'
   '';
 
   installPhase = ''
